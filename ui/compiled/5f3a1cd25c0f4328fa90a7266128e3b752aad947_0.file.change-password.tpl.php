b30d52bb08ea5f9af29dcd075c5eafd1ca30a132
Dhiraj-Pote <dhirajpote29@gmail.com>
2023-05-17 06:39:47 +0530

    billing

---
+++ ui/compiled/5f3a1cd25c0f4328fa90a7266128e3b752aad947_0.file.change-password.tpl.php
@@ -0,0 +1,100 @@
+<?php
+/* Smarty version 3.1.39, created on 2023-05-16 12:17:19
+  from 'C:\xampp\htdocs\ibilling\ui\theme\ibilling\change-password.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_6463ac8fe30852_01437380',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '5f3a1cd25c0f4328fa90a7266128e3b752aad947' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\ibilling\\ui\\theme\\ibilling\\change-password.tpl',
+      1 => 1621318798,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6463ac8fe30852_01437380 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_18860602476463ac8fe22255_61732490', "content");
+?>
+
+
+<?php $_smarty_tpl->inheritance->endChild($_smarty_tpl, ((string)$_smarty_tpl->tpl_vars['tpl_admin_layout']->value));
+}
+/* {block "content"} */
+class Block_18860602476463ac8fe22255_61732490 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_18860602476463ac8fe22255_61732490',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="ibox float-e-margins">
+                <div class="ibox-title">
+                    <h5><?php echo $_smarty_tpl->tpl_vars['_L']->value['Change Password'];?>
+</h5>
+
+                </div>
+                <div class="ibox-content">
+
+                    <form role="form" name="accadd" method="post" action="<?php echo $_smarty_tpl->tpl_vars['_url']->value;?>
+settings/change-password-post">
+                        <div class="form-group">
+                            <label for="password"><?php echo $_smarty_tpl->tpl_vars['_L']->value['Current Password'];?>
+</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                        </div>
+                        <div class="form-group">
+                            <label for="npass"><?php echo $_smarty_tpl->tpl_vars['_L']->value['New Password'];?>
+</label>
+                            <input type="password" class="form-control" id="npass" name="npass">
+                        </div>
+                        <div class="form-group">
+                            <label for="cnpass"><?php echo $_smarty_tpl->tpl_vars['_L']->value['Confirm New Password'];?>
+</label>
+                            <input type="password" class="form-control" id="cnpass" name="cnpass">
+                        </div>
+
+
+
+
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-check"></i> <?php echo $_smarty_tpl->tpl_vars['_L']->value['Submit'];?>
+</button>
+                    </form>
+
+                </div>
+            </div>
+
+
+
+        </div>
+
+
+
+    </div>
+
+
+<?php
+}
+}
+/* {/block "content"} */
+}
